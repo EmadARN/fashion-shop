c9af9a9305f64d946fa36b717b0a241456d62179
Amir <ahossein960@gmail.com>
2024-01-06 13:54:27 +0330

    Create Hr

---
+++ src/common/hr/HrStyl.jsx
@@ -0,0 +1,11 @@
+export const HrTypographyStyl = {
+    backgroundColor: "rgba(211, 211 , 211 , 0.4)",
+    width: "100%",
+    height: "100%",
+    color: "#DB4444",
+    marginRight: "3px",
+    borderRight: "25px solid #DB4444",
+    display: "flex",
+    alignItems: "center",
+    paddingRight: "5px",
+}
