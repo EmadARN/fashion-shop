823f7a7e5c9614a488c835ca03550808dc3c48f4
Amir <ahossein960@gmail.com>
2024-01-06 13:55:50 +0330

    Create Carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,12 @@ import Home from "@/components/home/Home";
 import Layout from "@/layout";
 import React from "react";
 
+
 const index = () => {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+<>
+
+</>
   );
 };
 
