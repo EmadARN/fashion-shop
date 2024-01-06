e2c396f41755e07c22418cf793bfad4f9333b4a6
Amir <ahossein960@gmail.com>
2024-01-06 14:01:18 +0330

    Update index.js & Rename CarouselMain

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import Home from "@/components/home/Home";
 import Layout from "@/layout";
 import React from "react";
+import CarouselMain from "@/components/home/Carousel/CarouselMain";
+
 
 const index = () => {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+
+<Layout>
+  <CarouselMain />
+</Layout>
+
   );
 };
 
