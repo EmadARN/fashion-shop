9ba1d2117b3a2c040707c7115eee05e6b6c2922c
Reza <rezafafaa78@gmail.com>
2024-01-07 12:55:50 +0330

    red main button has been maded

---
+++ src/common/redButton/style.jsx
@@ -0,0 +1,8 @@
+export const ButtonStyle = {
+    backgroundColor:"#DB4444" ,
+    color:"#fff",
+    borderRadius:"7px",
+    padding:'10px 15px',
+    fontSize:"18px",
+     '&:hover':{backgroundColor:"#f21f40"}
+}
