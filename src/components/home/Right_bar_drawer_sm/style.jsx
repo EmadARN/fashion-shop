089375858f04e29bffe9e59956c9fa17b884fa05
emad <arabanemad@gmail.com>
2024-01-15 14:03:00 +0330

    Update NavBar & styleRightBarXs

--- src/components/home/Right_bar_drawer_sm/style.jsx
+++ src/components/home/Right_bar_drawer_sm/style.jsx
@@ -1,11 +1,10 @@
-export  const IconeButton ={
-    bgcolor: "black",
-    position: "fixed",
-
-    borderRadius: "15",
-    right: "-1px",
-    zIndex: "100",
-    height: "35px",
-    boxShadow: "0 0 2px 10px #C1C1C1",
-    top: "13%",
-}
+export const IconeButton = {
+  bgcolor: "black",
+  position: "fixed",
+  borderRadius: "12px 0 0 12px",
+  right: "-1px",
+  zIndex: "100",
+  height: "35px",
+  boxShadow: "0 0 2px 10px #C1C1C1",
+  top: "13%",
+};
