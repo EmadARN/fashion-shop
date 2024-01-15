f8f376d73cc83f7e3a8c56dea768b03c151f0e98
Amir <ahossein960@gmail.com>
2024-01-15 12:34:54 +0330

    edit clean code & add style red to globall

--- src/components/home/Carousel/Carousel.jsx
+++ src/components/home/Carousel/Carousel.jsx
@@ -5,11 +5,15 @@ import { productimage } from "./data";
 import Img_data_grid from "./widgets/Img_data_grid";
 import { Responsive_carouselimg } from "./style";
 
-
 const Carousel = () => {
   return (
     <Box>
-      <Carousels responsive={Responsive_carouselimg} dotted={true} arrow={true} autoPlay={true}>
+      <Carousels
+        responsive={Responsive_carouselimg}
+        dotted={true}
+        arrow={true}
+        autoPlay={true}
+      >
         {productimage.map((item) => {
           return <Img_data_grid item={item} />;
         })}
