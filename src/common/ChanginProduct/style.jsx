cd5da935ca009c4a605226be098b551deac3e96e
Reza <rezafafaa78@gmail.com>
2024-01-20 19:05:25 +0330

    Changin_product has been maded

---
+++ src/common/ChanginProduct/style.jsx
@@ -0,0 +1,34 @@
+export const FirstmainGrid ={
+    boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+    padding:4,
+    mr:{xs:0,md:4},
+    ml:5 ,
+     display:"flex",
+     flexDirection:"column",
+      justifyContent:"center",
+     alignItems:"center"
+}
+
+
+export const FirstBox ={
+    borderBottom:"1px solid #ddd",width:"100%",mb:2,p:2
+}
+
+
+export const ImgStyle={
+    width:{xs:260,md:320},height:320
+}
+
+
+export const ButtonStyle={
+    width:"100%",fontSize:"18px",backgroundColor:"#fff",color:"#111",'&:hover':{backgroundColor:'#ddd'}
+}
+
+
+export const SeccondMainGrid ={
+    boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+    padding:4,
+    display:"flex",
+     flexDirection:"column",
+     mt:2
+}
