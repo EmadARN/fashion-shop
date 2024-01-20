8b7456441895ae11a2c9dd35912937487c2d42e5
emad <arabanemad@gmail.com>
2024-01-20 16:43:17 +0330

    Update Right_bar

--- src/components/home/Right_bar/style.jsx
+++ src/components/home/Right_bar/style.jsx
@@ -9,10 +9,24 @@ export const ListButton1 = {
   textAlign: "right",
   mb: 2,
   transition: "all 0.9s ease !important",
+  "& .iconhover": {
+    transition: "all 0.9s ease-in-out !important",
+  },
+  "& .iconhover3": {
+    transition: "all 0.9s ease-in-out !important",
+  },
   "&:hover": {
-    backgroundColor: "none !important",
-    "& .iconhover": {
+    backgroundColor: "#fff !important",
+    "& .iconhover ": {
       color: "red!important",
+      transform: "translateX(50%)",
+    },
+    "& .iconhover2 ": {
+      color: "red!important",
+    },
+    "& .iconhover3 ": {
+      color: "red!important",
+      transform: "translateX(50%) rotate(90deg)",
     },
     "& .texthover": {
       color: "red",
