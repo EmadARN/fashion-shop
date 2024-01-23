b8187a97917043603bd47ce77ba17f7e6c49b3b1
Amir <ahossein960@gmail.com>
2024-01-23 14:53:00 +0330

    edit Grid

--- src/common/signUp/registerForm_Information/RegistrForm_information.jsx
+++ src/common/signUp/registerForm_Information/RegistrForm_information.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, TextField } from "@mui/material";
+import { Box, Grid, TextField } from "@mui/material";
 import RTL from "@/common/rtl/Rtl";
 import { Textarea } from "@/components/contactUs/style";
 import TextArea from "./widget/Textarea";
@@ -7,91 +7,86 @@ import TextArea from "./widget/Textarea";
 const RegistrForm_information = () => {
   return (
     <RTL>
-      <Box>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            width: "110%",
-            pb:2,
-          }}
-        >
-          <TextField
-            sx={{
-              input: { color: "#efefef" },
-              "& label.MuiFormLabel-root": {
-                color: "#bcbcbc",
-                fontSize: "16px",
-                fontWeight: "bolder",
-              },
-              "& .MuiInput-underline:after": {
-                borderBottomColor: "#DB4444",
-              },
-            }}
-            id="standard-basic"
-            label="نام*"
-            variant="standard"
-          />
-          <TextField
-            sx={{
-              input: { color: "#efefef" },
-              "& label.MuiFormLabel-root": {
-                color: "#bcbcbc",
-                fontSize: "16px",
-                fontWeight: "bolder",
-              },
-              "& .MuiInput-underline:after": {
-                borderBottomColor: "#DB4444",
-              },
-            }}
-            id="standard-basic"
-            label="نام خانوادگی*"
-            variant="standard"
-          />
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            width: "110%",
-          }}
-        >
-          <TextField
-            sx={{
-              input: { color: "#efefef" },
-              "& label.MuiFormLabel-root": {
-                color: "#bcbcbc",
-                fontSize: "16px",
-                fontWeight: "bolder",
-              },
-              "& .MuiInput-underline:after": {
-                borderBottomColor: "#DB4444",
-              },
-            }}
-            id="standard-basic"
-            label="ایمیل*"
-            variant="standard"
-          />
-          <TextField
-            sx={{
-              input: { color: "#efefef" },
-              "& label.MuiFormLabel-root": {
-                color: "#bcbcbc",
-                fontSize: "16px",
-                fontWeight: "bolder",
-              },
-              "& .MuiInput-underline:after": {
-                borderBottomColor: "#DB4444",
-              },
-            }}
-            id="standard-basic"
-            label="نام فروشگاه*"
-            variant="standard"
-          />
-        </Box>
-        <Box sx={{pt:3}}>
-          <TextArea />
-        </Box>
+      <Box ml="25px">
+        <Grid container>
+          <Grid xs={12} sm={12} md={6} lg={6} xl={6}>
+            <TextField
+              sx={{
+                input: { color: "#efefef" },
+                "& label.MuiFormLabel-root": {
+                  color: "#bcbcbc",
+                  fontSize: "16px",
+                  fontWeight: "bolder",
+                },
+                "& .MuiInput-underline:after": {
+                  borderBottomColor: "#DB4444",
+                },
+              }}
+              id="standard-basic"
+              label="نام*"
+              variant="standard"
+            />
+          </Grid>
+          <Grid xs={12} sm={12} md={6} lg={6} xl={6}>
+            <TextField
+              sx={{
+                input: { color: "#efefef" },
+                "& label.MuiFormLabel-root": {
+                  color: "#bcbcbc",
+                  fontSize: "16px",
+                  fontWeight: "bolder",
+                },
+                "& .MuiInput-underline:after": {
+                  borderBottomColor: "#DB4444",
+                },
+              }}
+              id="standard-basic"
+              label="نام خانوادگی*"
+              variant="standard"
+            />
+          </Grid>
+
+          <Grid xs={12} sm={12} md={6} lg={6} xl={6}>
+            <TextField
+              sx={{
+                input: { color: "#efefef" },
+                "& label.MuiFormLabel-root": {
+                  color: "#bcbcbc",
+                  fontSize: "16px",
+                  fontWeight: "bolder",
+                },
+                "& .MuiInput-underline:after": {
+                  borderBottomColor: "#DB4444",
+                },
+              }}
+              id="standard-basic"
+              label="ایمیل*"
+              variant="standard"
+            />
+          </Grid>
+          <Grid xs={12} sm={12} md={6} lg={6} xl={6}>
+            <TextField
+              sx={{
+                input: { color: "#efefef" },
+                "& label.MuiFormLabel-root": {
+                  color: "#bcbcbc",
+                  fontSize: "16px",
+                  fontWeight: "bolder",
+                },
+                "& .MuiInput-underline:after": {
+                  borderBottomColor: "#DB4444",
+                },
+              }}
+              id="standard-basic"
+              label="نام فروشگاه*"
+              variant="standard"
+            />
+          </Grid>
+
+          <Box sx={{ pt: 3 }}>
+         
+          </Box>
+        </Grid>
       </Box>
     </RTL>
   );
