0fe0063080fc4644a49b2aabc97e4975af2695a4
Amir <ahossein960@gmail.com>
2024-01-23 14:25:37 +0330

    Create ButtonDesk & edit source code Desk_Button

---
+++ src/common/buttonDesk/Style.jsx
@@ -0,0 +1,7 @@
+export const Button_style = {
+  margin: "30px 20px 40px 0",
+  backgroundColor: "#333",
+  fontSize: { xs: "11px", sm: "12px", lg: "16px" },
+  "&:hover": { backgroundColor: "#777" },
+
+};
