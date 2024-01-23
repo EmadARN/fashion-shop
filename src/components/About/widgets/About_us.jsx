f3f8fcdaeb7eda7e7ff9ddd7ed3defb95c933032
Reza <rezafafaa78@gmail.com>
2024-01-23 13:16:20 +0330

    About Us page completed

--- src/components/About/widgets/About_us.jsx
+++ src/components/About/widgets/About_us.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Button, Grid, Box, Typography } from "@mui/material";
+import { Grid, Box, Typography } from "@mui/material";
+import {
+  DescTypography,
+  ContainerGridStyle,
+  FirstGridStyle,
+  FirstBoxStyle,
+  TitleTypography,
+  ImgStyle,
+  SeccondBoxStyle,
+} from "../style";
 const About_us = () => {
   return (
-    <Grid container mt={4} flexDirection="column" justifyContent="center" alignItems="center">
-      <Grid xs={12}   mb={5} display="flex" justifyContent="center" alignItems="center" width="100%" >
-        <Box ml={5} display="flex" flexDirection="column" width="100%" maxWidth="25%">
-          <Typography sx={{fontSize:"24px",fontWeight:"bold"}}>درباره ی ما</Typography>
-          <Typography sx={{textAlign:"justify" ,fontSize:"17px"}}>
+    <Grid sx={ContainerGridStyle}>
+      <Grid xs={12} mb={5} sx={FirstGridStyle}>
+        <Box sx={FirstBoxStyle}>
+          <Typography sx={TitleTypography}>درباره ی ما</Typography>
+          <Typography sx={DescTypography}>
             لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
             استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
             ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
@@ -25,22 +34,22 @@ const About_us = () => {
           <img
             src="https://d2w9m16hs9jc37.cloudfront.net/dimg/blog/2023/09/hockerty_dark_grey_overcoat_over_a_suit_abeab026_600f_4557_85d1_65742fbb21e9.png"
             alt=""
-            style={{ width: 300, height: 300 }}
+            style={ImgStyle}
           />
         </Box>
       </Grid>
 
-      <Grid xs={12} display="flex" justifyContent="center" alignItems="center" width="100%" >
+      <Grid sx={FirstGridStyle}>
         <Box>
           <img
             src="https://imgs7.luluandsky.com/catalog/product/8/9/89TGC-1151-KHAKI_1.jpeg"
             alt=""
-            style={{ width: 300, height: 300 }}
+            style={ImgStyle}
           />
         </Box>
-        <Box mr={5} display="flex" flexDirection="column" width="100%" maxWidth="25%">
-          <Typography sx={{fontSize:"24px",fontWeight:"bold"}}> هدف ما</Typography>
-          <Typography  sx={{textAlign:"justify" ,fontSize:"17px"}}>
+        <Box sx={SeccondBoxStyle}>
+          <Typography sx={TitleTypography}> هدف ما</Typography>
+          <Typography sx={DescTypography}>
             لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
             استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
             ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
---
+++ src/pages/About_Us.jsx
@@ -0,0 +1,10 @@
+import About_Main from '@/components/About/Main'
+import React from 'react'
+
+const About_Us = () => {
+  return (
+   <About_Main/>
+  )
+}
+
+export default About_Us
