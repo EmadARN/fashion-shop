ce4e814d39a1dbb2919e0e2575ced398e33e1150
emad <arabanemad@gmail.com>
2024-01-24 13:25:59 +0330

    Create scrollbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import MainRegisterForm from "@/components/signUp/MainRegisterForm";
 import MainFormVerifyCode from "@/components/signUp/MainFormVerifyCode";
 import MainFormVerifyNumber from "@/components/signUp/MainFormVerifyNumber";
+import Cards from "@/common/cards/Cards";
+import Home from "@/components/home/Home";
+import Layout from "@/layout";
 
 const index = () => {
   return (
-    <>
-      <MainRegisterForm></MainRegisterForm>
-    </>
+    <Layout>
+      <Home></Home>
+    </Layout>
   );
 };
 
