de1c9b8cb059873dfe5819871fd5365947412778
Reza <rezafafaa78@gmail.com>
2024-01-27 17:01:45 +0330

    index js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import MainRegisterForm from "@/components/signUp/MainRegisterForm";
 import MainFormVerifyCode from "@/components/signUp/MainFormVerifyCode";
 import MainFormVerifyNumber from "@/components/signUp/MainFormVerifyNumber";
-import Cards from "@/common/cards/Cards";
+
 import Home from "@/components/home/Home";
 import Layout from "@/layout";
 
 const index = () => {
-  return <MainFormVerifyCode></MainFormVerifyCode>;
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
 };
 
 export default index;
