ca031c0c22e64a066b9d47b7d54e2aaf60d33b0c
Reza <rezafafaa78@gmail.com>
2024-02-04 11:55:36 +0330

    tooltip added to Button Desk

--- src/common/buttonDesk/ButtonDesk.jsx
+++ src/common/buttonDesk/ButtonDesk.jsx
@@ -1,17 +1,19 @@
 import { Box, Button, Grid } from "@mui/material";
 import { Button_style, Button_style1 } from "./Style";
 import { CiSettings } from "react-icons/ci";
-
+import Tooltip from '@mui/material/Tooltip';
 function ButtonDesk() {
   return (
     <>
       <Grid container sx={{ width: "50%", mb: 0 }}>
         <Grid xs={2}>
+          <Tooltip title="ویرایش پروفایل">
           <Button
             sx={Button_style1}
             variant="contained"
-            startIcon={<CiSettings color="#fff" />}
+            startIcon={<CiSettings color="#111" />}
           ></Button>
+          </Tooltip>
         </Grid>
         <Grid xs={5}>
           <Button sx={Button_style} variant="contained">
--- src/common/buttonDesk/Style.jsx
+++ src/common/buttonDesk/Style.jsx
@@ -1,17 +1,21 @@
 export const Button_style = {
   margin: "10px auto 10px auto",
-  backgroundColor: "#333",
+  backgroundColor: "#ddd",
   fontSize: { xs: "11px", sm: "12px", lg: "16px" },
+  color:"#111",
+  transition:"all 0.5s ease",
   "&:hover": { backgroundColor: "#777" },
   width: "95%",
   whiteSpace: "nowrap",
+
   px: 5,
 };
 
 export const Button_style1 = {
   width: "auto",
+  backgroundColor: "#ddd",
   margin: "10px auto 10px auto",
-  backgroundColor: "#333",
+  color:"#111",
   fontSize: { xs: "5px", sm: "12px", lg: "16px", xl: "25" },
   "&:hover": { backgroundColor: "#777" },
   px: 7,
