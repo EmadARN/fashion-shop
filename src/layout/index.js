01ff6af8e68b067f085b329a334ce30aa90c797e
emad <arabanemad@gmail.com>
2024-02-04 11:46:46 +0330

    Update NavFooter

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,14 +1,18 @@
 import FooterMd from "./Footer/FooterMd";
 import FooterXs from "./Footer/FooterXs";
 import NavBar from "./NavBar/NavBar";
-
+import { usePathname } from "next/navigation";
 const Layout = ({ children }) => {
+  const pathname = usePathname();
+  let showNavFooter = true;
+  if (pathname === "/signIn") {
+    showNavFooter = false;
+  }
   return (
     <div>
-      <NavBar />
+      {showNavFooter && <NavBar />}
       {children}
-      <FooterMd />
-      <FooterXs />
+      {showNavFooter && <FooterMd />} {showNavFooter && <FooterXs />}
     </div>
   );
 };
--- src/pages/About_Us.js
+++ src/pages/About_Us.js
@@ -1,10 +1,9 @@
-import About_Main from '@/components/About/Main'
-import React from 'react'
+import About_Main from "@/components/About/Main";
+import Layout from "@/layout";
+import React from "react";
 
 const about_Us = () => {
-  return (
-   <About_Main/>
-  )
-}
+  return <About_Main />;
+};
 
-export default about_Us
+export default about_Us;
