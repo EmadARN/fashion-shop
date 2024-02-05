e6e9a26aeae4f78235fdad536fba40e6da992071
Reza <rezafafaa78@gmail.com>
2024-02-05 15:52:26 +0330

    index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Home from "@/components/home/Home";
+import Main_Desk from "@/components/Desk/Main";
+import Information_main from "@/components/information_store/main";
 
 const index = () => {
-  return <Home />;
+  return <Main_Desk />;
 };
 
 export default index;
