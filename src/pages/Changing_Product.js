df4aa05ebdb3b7b422d9e1b2ab24a121bff89c97
Reza <rezafafaa78@gmail.com>
2024-02-15 13:18:52 +0330

    Link added to footer and navbar

---
+++ src/pages/Changing_Product.js
@@ -0,0 +1,10 @@
+import Changin_Product_Main from '@/common/ChanginProduct/Index'
+import React from 'react'
+
+const Changing_Product = () => {
+  return (
+    <Changin_Product_Main/>
+  )
+}
+
+export default Changing_Product
