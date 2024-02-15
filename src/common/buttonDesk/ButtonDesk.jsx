5b3aaa118881a5795f66e506edb290e8bf0793e1
Reza <rezafafaa78@gmail.com>
2024-02-15 13:16:36 +0330

    Modal added to dESK PAGE

--- src/common/buttonDesk/ButtonDesk.jsx
+++ src/common/buttonDesk/ButtonDesk.jsx
@@ -3,13 +3,20 @@ import { Button_style, Button_style1, Button_style2, oneBox } from "./Style";
 import { CiSettings } from "react-icons/ci";
 import Tooltip from "@mui/material/Tooltip";
 import LabTabs from "@/components/labTab/LabTabs";
+import ModalMain from "@/components/information_store/widgets/Modal";
+import { useState } from "react";
 function ButtonDesk() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
   return (
     <Container maxWidth="md">
+      {open && <ModalMain open={open}  setOpen = {setOpen}/>}
       <Grid container sx={oneBox}>
         <Grid >
           <Tooltip title="ویرایش پروفایل">
             <Button
+             onClick={handleOpen}
               sx={Button_style1}
               variant="contained"
               startIcon={<CiSettings color="#111" />}
