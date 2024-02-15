f30662ac4a20381e9f3d639a67d2f9342ca0f14d
emad <arabanemad@gmail.com>
2024-02-15 11:11:04 +0330

    Create

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import Home from "@/components/home/Home";
 import Main_Desk from "@/components/Desk/Main";
 import Information_main from "@/components/information_store/main";
+import CustomerViewDesk from "@/components/CustomerViewDesk/main";
 
 const index = () => {
-  return <Main_Desk />;
+  return <CustomerViewDesk />;
 };
 
 export default index;
